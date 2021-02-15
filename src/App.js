04f2abd56bb6c67753934cc638e42a5a597f557e
aludcar <aludcar@gmail.com>
2021-02-15 18:05:16 -0500

    Clicker

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
 import './App.css';
+import Counter from './Counter'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <Button onClick={() => console.log("Clicky!")}>Test</Button>
-      </header>
+const [valCounter, setValCounter] = useState(0)
+
+function interactionCounter(reset, value) {
+  value++;
+  if(reset){
+    value = 0;
+  }
+  setValCounter(value);
+}
+  //Pendiente de agregar el grid para hacerlo responsive
+    return ( 
+    <div className = "App" >
+      <header className = "App-header" >
+        <Counter value={valCounter} ></Counter>
+        <Button onClick = {() => interactionCounter(false, valCounter)} > Click </Button> 
+        <Button onClick = {() => interactionCounter(true, valCounter)} > Reset </Button> 
+      </header> 
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,10 @@
+
+function Counter({value}){
+    return (
+        <>
+            <h1>The count is {value}</h1>
+        </>
+    );
+}
+
+export default Counter;
